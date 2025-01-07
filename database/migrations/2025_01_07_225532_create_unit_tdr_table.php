97b14f4b0534f770fdd9322229aeefe518cbbec8
lipik <a.lipikhin@gmail.com>
2025-01-07 18:26:20 -0500

    Add: -UnitTDR -UnitTDR-migration -Code, migration

---
+++ database/migrations/2025_01_07_225532_create_unit_tdr_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unit_tdr', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+        });
+
+
+        $csvFile = public_path('data/unit_tdr.csv');
+        $file = fopen($csvFile, 'r');
+        $headers = fgetcsv($file);
+        while (($row = fgetcsv($file)) !== false) {
+            DB::table('unit_tdr')->insert([
+                'name' => $row[0],
+            ]);
+        }
+        fclose($file);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('unit_tdr');
+    }
+};
---
+++ database/migrations/2025_01_07_231839_create_code_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('code', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('code');
+        });
+
+        $csvFile = public_path('data/code.csv');
+        $file = fopen($csvFile, 'r');
+        $headers = fgetcsv($file);
+        while (($row = fgetcsv($file)) !== false) {
+            DB::table('unit_tdr')->insert([
+                'name' => $row[0],
+                'code' => $row[1],
+            ]);
+        }
+        fclose($file);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('code');
+    }
+};
