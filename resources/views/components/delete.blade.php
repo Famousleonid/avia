b4392897b62ce652662dd93eb0a6287042ddeb8c
leonid <69190810+Famousleonid@users.noreply.github.com>
2025-01-19 18:53:40 -0500

    Add edit & delete workorders

--- resources/views/components/delete.blade.php
+++ resources/views/components/delete.blade.php
@@ -1,6 +1,6 @@
 <div class="modal fade" id="useConfirmDelete" tabindex="-1" aria-labelledby="confirmDeleteLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
+    <div class="modal-dialog ">
+        <div class="modal-content bg-gradient">
             <div class="modal-header">
                 <h5 class="modal-title" id="confirmDeleteLabel">Delete Confirmation</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
