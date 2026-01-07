24f78ad9d4784e02db4a8ea6cc711053636b3ef6
lipik <a.lipikhin@gmail.com>
2026-01-07 17:13:47 -0500

    delete use_log_card & use_extra_forms from tab tdrs. upd TdrController and tdrs.show - no null data with tdr-missing

---
+++ database/migrations/2026_01_07_165922_remove_use_log_card_and_use_extra_forms_from_tdrs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tdrs', function (Blueprint $table) {
+            $table->dropColumn(['use_log_card', 'use_extra_forms']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tdrs', function (Blueprint $table) {
+            $table->boolean('use_log_card')->default(false);
+            $table->boolean('use_extra_forms')->default(false);
+        });
+    }
+};
+
